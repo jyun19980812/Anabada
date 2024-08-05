b7b537142a4197ebd466a0385fe569a289faaa2c
MangoInfantry <spacewather@naver.com>
2024-08-05 15:58:31 +0900

    home,information,main,points,reward_dart refactoring

--- anabada/lib/home.dart
+++ anabada/lib/home.dart
@@ -9,9 +9,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final fontSizeProvider = Provider.of<FontSizeProvider>(context);
-    const double baseFontSize = 20.0; // 폰트 S M L 용도 -> 기본 크기 바꾸고 싶으면 여기서 조정
-    final double cardHeight = MediaQuery.of(context).size.height * 0.55; // 화면 높이의 40%로 카드 높이 조정
-    final double cardWidth = MediaQuery.of(context).size.width - 32; // 패딩을 제외한 너비
+    const double baseFontSize = 20.0;
+    final double cardHeight = MediaQuery.of(context).size.height * 0.55;
+    final double cardWidth = MediaQuery.of(context).size.width - 32;
 
     return Scaffold(
       body: SingleChildScrollView(
@@ -19,218 +19,124 @@ class HomeScreen extends StatelessWidget {
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
-              GestureDetector(
-                onTap: () {
-                  onTabTapped(2); // Recycle 탭으로 이동
-                },
-                child: Card(
-                  color: const Color(0xFF009E73),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  ),
-                  child: Container(
-                    height: cardHeight,
-                    width: cardWidth,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          const Icon(Icons.recycling, size: 50, color: Colors.white),
-                          const SizedBox(height: 10),
-                          Text(
-                            'Points Available!',
-                            style: TextStyle(
-                                fontSize: fontSizeProvider.getFontSize(baseFontSize + 4.0),
-                                color: Colors.white),
-                            maxLines: 1,
-                          ),
-                          const SizedBox(height: 10),
-                          Text(
-                            'Take a photo, recycle, and earn up to 100 points today!',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontSize: fontSizeProvider.getFontSize(baseFontSize),
-                                color: Colors.white),
-                            maxLines: 2,
-                          ),
-                          const SizedBox(height: 10),
-                          Text(
-                            'Recycle!',
-                            style: TextStyle(
-                              fontSize: fontSizeProvider.getFontSize(baseFontSize),
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                            maxLines: 1,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
+              _buildCard(
+                context: context,
+                icon: Icons.recycling,
+                title: 'Points Available!',
+                subtitle: 'Take a photo, recycle, and earn up to 100 points today!',
+                actionText: 'Recycle!',
+                onTap: () => onTabTapped(2),
+                fontSizeProvider: fontSizeProvider,
+                cardHeight: cardHeight,
+                cardWidth: cardWidth,
+                baseFontSize: baseFontSize,
               ),
               const SizedBox(height: 16),
-              GestureDetector(
-                onTap: () {
-                  onTabTapped(1); // Reward 탭으로 이동
-                },
-                child: Card(
-                  color: const Color(0xFF009E73),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  ),
-                  child: Container(
-                    height: cardHeight,
-                    width: cardWidth,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          const Icon(Icons.card_giftcard, size: 50, color: Colors.white),
-                          const SizedBox(height: 10),
-                          Text(
-                            'Want Reward?',
-                            style: TextStyle(
-                                fontSize: fontSizeProvider.getFontSize(baseFontSize + 4.0),
-                                color: Colors.white),
-                            maxLines: 1,
-                          ),
-                          const SizedBox(height: 10),
-                          Text(
-                            'Spend your points for gift cards!',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontSize: fontSizeProvider.getFontSize(baseFontSize),
-                                color: Colors.white),
-                            maxLines: 2,
-                          ),
-                          const SizedBox(height: 10),
-                          Text(
-                            'Reward!',
-                            style: TextStyle(
-                              fontSize: fontSizeProvider.getFontSize(baseFontSize),
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                            maxLines: 1,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
+              _buildCard(
+                context: context,
+                icon: Icons.card_giftcard,
+                title: 'Want Reward?',
+                subtitle: 'Spend your points for gift cards!',
+                actionText: 'Reward!',
+                onTap: () => onTabTapped(1),
+                fontSizeProvider: fontSizeProvider,
+                cardHeight: cardHeight,
+                cardWidth: cardWidth,
+                baseFontSize: baseFontSize,
               ),
               const SizedBox(height: 16),
-              GestureDetector(
-                onTap: () {
-                  onTabTapped(3); // Points 탭으로 이동
-                },
-                child: Card(
-                  color: const Color(0xFF009E73),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  ),
-                  child: Container(
-                    height: cardHeight,
-                    width: cardWidth,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          const Icon(Icons.point_of_sale, size: 50, color: Colors.white),
-                          const SizedBox(height: 10),
-                          Text(
-                            'How Are You Doing?',
-                            style: TextStyle(
-                                fontSize: fontSizeProvider.getFontSize(baseFontSize),
-                                color: Colors.white),
-                            maxLines: 1,
-                          ),
-                          const SizedBox(height: 10),
-                          Text(
-                            'See where you are at with your points!',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontSize: fontSizeProvider.getFontSize(baseFontSize),
-                                color: Colors.white),
-                            maxLines: 2,
-                          ),
-                          const SizedBox(height: 10),
-                          Text(
-                            'Check!',
-                            style: TextStyle(
-                              fontSize: fontSizeProvider.getFontSize(baseFontSize),
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                            maxLines: 1,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
+              _buildCard(
+                context: context,
+                icon: Icons.point_of_sale,
+                title: 'How Are You Doing?',
+                subtitle: 'See where you are at with your points!',
+                actionText: 'Check!',
+                onTap: () => onTabTapped(3),
+                fontSizeProvider: fontSizeProvider,
+                cardHeight: cardHeight,
+                cardWidth: cardWidth,
+                baseFontSize: baseFontSize,
               ),
               const SizedBox(height: 16),
-              GestureDetector(
-                onTap: () {
-                  onTabTapped(4); // Information 탭으로 이동
-                },
-                child: Card(
-                  color: const Color(0xFF009E73),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                  ),
-                  child: Container(
-                    height: cardHeight,
-                    width: cardWidth,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          const Icon(Icons.info, size: 50, color: Colors.white),
-                          const SizedBox(height: 10),
-                          Text(
-                            'Curious About Recycle?',
-                            style: TextStyle(
-                                fontSize: fontSizeProvider.getFontSize(baseFontSize),
-                                color: Colors.white),
-                            maxLines: 1,
-                          ),
-                          const SizedBox(height: 10),
-                          Text(
-                            'Learn what and how to recycle!',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                fontSize: fontSizeProvider.getFontSize(baseFontSize),
-                                color: Colors.white),
-                            maxLines: 2,
-                          ),
-                          const SizedBox(height: 10),
-                          Text(
-                            'Learn!',
-                            style: TextStyle(
-                              fontSize: fontSizeProvider.getFontSize(baseFontSize),
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                            maxLines: 1,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
+              _buildCard(
+                context: context,
+                icon: Icons.info,
+                title: 'Curious About Recycle?',
+                subtitle: 'Learn what and how to recycle!',
+                actionText: 'Learn!',
+                onTap: () => onTabTapped(4),
+                fontSizeProvider: fontSizeProvider,
+                cardHeight: cardHeight,
+                cardWidth: cardWidth,
+                baseFontSize: baseFontSize,
               ),
             ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCard({
+    required BuildContext context,
+    required IconData icon,
+    required String title,
+    required String subtitle,
+    required String actionText,
+    required VoidCallback onTap,
+    required FontSizeProvider fontSizeProvider,
+    required double cardHeight,
+    required double cardWidth,
+    required double baseFontSize,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        color: const Color(0xFF009E73),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: Container(
+          height: cardHeight,
+          width: cardWidth,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(icon, size: 50, color: Colors.white),
+                const SizedBox(height: 10),
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: fontSizeProvider.getFontSize(baseFontSize + 4.0),
+                    color: Colors.white,
+                  ),
+                  maxLines: 1,
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  subtitle,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: fontSizeProvider.getFontSize(baseFontSize),
+                    color: Colors.white,
+                  ),
+                  maxLines: 2,
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  actionText,
+                  style: TextStyle(
+                    fontSize: fontSizeProvider.getFontSize(baseFontSize),
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                  maxLines: 1,
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- anabada/lib/main.dart
+++ anabada/lib/main.dart
@@ -23,7 +23,7 @@ import 'account/account.dart';
 import 'settings/settings.dart';
 import 'settings/font_size_provider.dart';
 import 'settings/image_provider.dart';
-
+import 'package:flutter/services.dart';
 
 // Your other imports
 import 'package:flutter/material.dart';
@@ -32,7 +32,10 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeFirebase();
   await FirebaseAppCheck.instance.activate();
-
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(MultiProvider(
     providers:[
       ChangeNotifierProvider(create: (_) => FontSizeProvider()),
